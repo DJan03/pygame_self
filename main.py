3d3c2abd10d29f4117988a45bcfec5f2c45af681
DJan03 <danil2003nsk@gmail.com>
2020-06-05 17:15:01 +0300

    Better world collision

--- main.py
+++ main.py
@@ -6,6 +6,86 @@ window_size = (1280, 720)
 window = pygame.display.set_mode(window_size)
 pygame.display.set_caption("WhiteLight_remake")
 
+ROOM_EMPTY = 1
+ROOM_X = 2
+
+room_lib = {
+    ROOM_EMPTY: [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ],
+    ROOM_X: [
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1],
+        [1, 0, 0, 1, 1, 1, 0, 0, 1],
+        [1, 0, 0, 1, 1, 1, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    ]
+}
+
+
+class World:
+    def __init__(self):
+        self.rooms = [
+            [None, None, None],
+            [room_lib[ROOM_EMPTY], room_lib[ROOM_X], room_lib[ROOM_EMPTY]],
+            [None, room_lib[ROOM_X], None]
+        ]
+        self.x = 1
+        self.y = 1
+
+        self.size = 80
+
+    def get_room(self):
+        return self.rooms[self.x][self.y]
+
+    def is_collised(self, x, y, w, h):
+        x0 = x // 80
+        y0 = y // 80
+        x1 = (x + w - 1) // 80
+        y1 = (y + h - 1) // 80
+
+        room = self.rooms[self.x][self.y]
+        return (room[x0][y0] == 0 and room[x1][y0] == 0 and
+                room[x0][y1] == 0 and room[x1][y1] == 0)
+
+    def draw(self):
+        room = list(self.rooms[self.x][self.y])
+
+        for i in range(len(room)):
+            for j in range(len(room[0])):
+                if (room[i][j] == 1):
+                    pygame.draw.rect(window, (255, 0, 0), (i * self.size, j * self.size, self.size, self.size))
+
+
+world = World()
+
+
 class Player:
     def __init__(self, x, y, width, height, speed):
         self.x = x
@@ -23,8 +103,9 @@ class Player:
         newX = self.x + dx
         newY = self.y + dy
 
-        self.x = newX
-        self.y = newY
+        if (world.is_collised(newX, newY, self.width, self.height)):
+            self.x = newX
+            self.y = newY
 
     def draw(self):
         pygame.draw.rect(
@@ -65,6 +146,7 @@ while (run):
 
     window.fill((0, 0, 0))
 
+    world.draw()
     player.draw()
     pygame.display.update()
 
